3aba4bdd5719337003ab00e90e96fbee52aec59a
Paul-Okello <paulokello273@gmail.com>
2021-10-12 10:54:01 +0300

    💥 NEW: Footer Component

---
+++ components/Footer.jsx
@@ -0,0 +1,40 @@
+const Footer = () => {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>ABOUT</h5>
+        <p className=''>How Airbnb works</p>
+        <p className=''>Newsroom</p>
+        <p className=''>Investors</p>
+        <p className=''>Airbnb Plus</p>
+        <p className=''>Airbnb Luxe</p>
+      </div>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>COMMUNITY</h5>
+        <p className=''>Accessibility</p>
+        <p className=''>Not a real site</p>
+        <p className=''>Referals Accepted</p>
+        <p className=''>Nextjs</p>
+        <p className=''>Airbnb Luxe</p>
+      </div>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>HOST</h5>
+        <p className=''>Lorem sit amet</p>
+        <p className=''>ipsum dolor</p>
+        <p className=''>adipisicing elit</p>
+        <p className=''>quibusdam molestias</p>
+        <p className=''> maxime id?</p>
+      </div>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>SUPPORT</h5>
+        <p className=''>tenetur laudantium</p>
+        <p className=''>voluptatem veniam</p>
+        <p className=''>beatae eveniet</p>
+        <p className=''>placeat modi</p>
+        <p className=''>Deleniti explicabo</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
