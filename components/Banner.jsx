01e614a2adc39083fcf10db9e0e8099ea4db3bae
Paul-Okello <paulokello273@gmail.com>
2021-10-20 12:07:33 +0300

    🐛 FIX: font

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -9,8 +9,10 @@ const Banner = () => {
         objectFit='cover'
       />
       <div className='absolute top-1/2 w-full text-center'>
-        <p className='text-sm sm:text-lg'>Not Sure where to go? Perfect</p>
-        <button className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>
+        <p className='text-sm sm:text-lg font-poppins font-bold'>
+          Not Sure where to go? Perfect
+        </p>
+        <button className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150 '>
           I'm Flexible
         </button>
       </div>
