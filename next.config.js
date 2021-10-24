0969daa9faa35dcd54e3844d44eb515bf6e0cb73
Paul-Okello <paulokello273@gmail.com>
2021-10-24 09:09:31 +0300

    ⬆️ Dependencies

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoicGF1bG9jaGllbmciLCJhIjoiY2t2NHF2YXFmMWQ0eDJ2b3ZzazQ3NGxpOSJ9.kdTuK0eRy2HN0UTne_vC2w",
+  },
 };
